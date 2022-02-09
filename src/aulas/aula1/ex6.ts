420602f749b56a712748c7d78dad01dfe7c402b1
Esther Massarioli <esther.massarioli@luizalabs.com>
2022-02-09 19:16:03 -0300

    exerc aula 1

--- src/aulas/aula1/ex6.ts
+++ src/aulas/aula1/ex6.ts
@@ -5,5 +5,7 @@
  * Faça a declaração da função.
  * Troque "{}" pelo nome dado a função
  */
-
-export default {};
+function soma(x: number, y: number): number {
+  return Number(x) + Number(y);
+}
+export default soma;
